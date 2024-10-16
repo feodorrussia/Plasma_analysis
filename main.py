070ed736675de3bb67fc2ed8ed84335292da798d
Fyodor <34249952+feodorrussia@users.noreply.github.com>
2024-10-16 18:17:35 +0300

    Write program to copy DBS sht from DB

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import shutil
+import os
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+init_path = "D:/Work/Data/dbs/"
+destination_path = "C:/Users/f.belous/Work/Projects/Plasma_analysis/data/dbs/"
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+count = 0
+for date_dir in os.listdir(init_path):
+    if os.path.isfile(date_dir):
+        continue
+    for shot_dir in os.listdir(init_path + date_dir + "/"):
+        if os.path.isfile(shot_dir):
+            continue
+        for file in os.listdir(init_path + date_dir + "/" + shot_dir + "/"):
+            if os.path.isfile(init_path + date_dir + "/" + shot_dir + "/" + file):
+                start_time = time.time()
+                if os.path.splitext(file)[-1].lower() == ".sht" and not os.path.exists(destination_path + "sht/" + file):
+                    shutil.copyfile(init_path + date_dir + "/" + shot_dir + "/" + file, destination_path + "sht/" + file)
+                if os.path.splitext(file)[-1].lower() == ".dat" and not os.path.exists(destination_path + "dat/" + file):
+                    shutil.copyfile(init_path + date_dir + "/" + shot_dir + "/" + file, destination_path + "dat/" + file)
+                    
+                if os.path.splitext(file)[-1].lower() == ".sht" or os.path.splitext(file)[-1].lower() == ".dat":
+                    count += 1
+                    print(count, "-", init_path + date_dir + "/" + shot_dir + "/" + file, end="")
+                    print(f" - {os.path.getsize(init_path + date_dir + '/' + shot_dir + '/' + file) / 2 ** 20:.3f} MB", end="")
+                    print(f" - Tooks: {(time.time() - start_time):.3f} ms")
